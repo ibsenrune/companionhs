47e2f6ab7f21f650dd1aee4d19d9bebba273b7cf
Rune Ibsen <rui@copenhagensoftware.com>
2017-07-16 14:49:56 +0200

    Implement input loop

--- signup.hs
+++ signup.hs
@@ -1,8 +1,10 @@
 import Data.List
+import Data.Maybe
 
 data Record = Record { state :: String, partnerId :: String, userId :: String }
     deriving (Show, Read)
 data RecordIdentifier = PartnerId String | UserId String
+    deriving (Show, Read)
 
 data UserState =
     NonExistent
@@ -10,7 +12,7 @@ data UserState =
     | Pending
     | Active
     | Suspended
-    deriving (Show, Eq)
+    deriving (Show, Eq, Read)
 
 data Command = Command UserState
 
@@ -29,10 +31,6 @@ transition Pending     (Command Active)    = Just Active
 transition Active      (Command Suspended) = Just Suspended
 transition _           (Command _)         = Nothing
 
-saveRecords :: [Record] -> IO ()
-saveRecords rs =
-    do writeFile "store.txt" (show rs)
-
 findRecord :: RecordIdentifier -> [Record] -> Maybe Record
 findRecord (PartnerId pi) = find (\r -> partnerId r == pi) 
 findRecord (UserId ui) = find (\r -> userId r == ui) 
@@ -42,3 +40,43 @@ loadRecord id =
     do txt <- readFile "store.txt"
        let records = read txt :: [Record]
        return (findRecord id records)
+
+saveRecords :: [Record] -> IO ()
+saveRecords rs =
+    do writeFile "store.txt" (show rs)
+
+data Input = Input RecordIdentifier UserState deriving (Show, Read)
+
+inputLoop :: IO ()
+inputLoop = do
+    putStrLn "Tell me what to do:"
+    command <- getLine
+    if(command == ":q") 
+        then  
+            do return ()
+        else
+            do
+                let Input id targetState = read command :: Input
+                record <- loadRecord id
+                if(not (isJust record)) 
+                    then do return ()
+                    else 
+                        do
+                            let cmd = Command targetState
+                            let r = fromJust record
+                            let startState = toState r
+                            let endState = transition startState cmd
+                            if(not (isJust endState))
+                                then do
+                                    putStrLn "Cannot transition like that"
+                                    inputLoop
+                                else do
+                                    putStrLn ("Action: " ++ (show (Input id targetState)))
+                                    inputLoop
+    
+     
+
+
+
+
+
