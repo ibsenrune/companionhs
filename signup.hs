77ce35bda279e835290419820a3b1d646e07eed1
Rune Ibsen <rui@copenhagensoftware.com>
2018-04-01 16:59:09 +0200

    Extract to separate files types.hs and db.hs
    types.hs: business domain types.
    
    db.hs: functions for reading and writing to file store.

--- signup.hs
+++ signup.hs
@@ -2,39 +2,11 @@ import Data.List
 import Data.Maybe
 import Data.Function
 import Control.Monad
-
-data Record = Record { state :: String, partnerId :: String, userId :: String }
-    deriving (Show, Read)
-data RecordIdentifier = PartnerId String | UserId String
-    deriving (Show, Read)
-
-data UserState =
-    NonExistent
-    | Inactive
-    | Pending
-    | Active
-    | Suspended
-    deriving (Show, Eq, Read)
+import Types
+import DB
 
 data Command = Command UserState
 data Input = Input RecordIdentifier UserState deriving (Show, Read)
-
-parseState :: String -> UserState
-parseState "NonExistent" = NonExistent
-parseState "Inactive" = Inactive
-parseState "Pending" = Pending
-parseState "Active" = Active
-parseState "Suspended" = Suspended
-
-toState :: Record -> UserState
-toState = parseState . state
-
-toStateString :: UserState -> String
-toStateString Active = "Active"
-toStateString Inactive = "Inactive"
-toStateString Pending = "Pending"
-toStateString NonExistent = "NonExistent"
-toStateString Suspended = "Suspended"
 
 transition :: UserState -> Command -> Maybe UserState
 transition NonExistent (Command Inactive)  = Just Inactive
@@ -44,56 +16,16 @@ transition Pending     (Command Active)    = Just Active
 transition Active      (Command Suspended) = Just Suspended
 transition _           (Command _)         = Nothing
 
-findRecord :: RecordIdentifier -> [Record] -> Maybe Record
-findRecord (PartnerId pi) = find (\r -> partnerId r == pi) 
-findRecord (UserId ui) = find (\r -> userId r == ui) 
-
-loadRecords :: () -> IO ([Record])
-loadRecords _ = do 
-        txt <- readFile "store.txt"
-        let records = read txt :: [Record]
-        return records
-
-loadRecord :: RecordIdentifier -> IO (Maybe Record)
-loadRecord id = do
-    records <- loadRecords ()
-    return (findRecord id records)
-
-saveRecords :: [Record] -> IO ()
-saveRecords rs =
-    do writeFile "store.txt" (show rs)
-
-updateRecord :: [Record] -> RecordIdentifier -> UserState -> [Record]
-updateRecord [] _ _ = []
-updateRecord all@(r@(Record { partnerId = rpid }):rest) (PartnerId pid) state 
-    | rpid == pid = (r { state = toStateString state }) : rest 
-updateRecord all@(r@(Record{ userId = ruid }):rest) (UserId uid) state 
-    | ruid == uid = (r { state = toStateString state }) : rest 
-updateRecord (r:rest) id state = r : (updateRecord rest id state)
-
-updateRecords :: ([Record] -> [Record]) -> () -> IO ()
-updateRecords f _ = do
-    records <- loadRecords ()
-    let updatedRecords = f records
-    saveRecords updatedRecords
-
-writeNewState :: RecordIdentifier -> UserState -> IO ()
-writeNewState id state = updateRecords (\records -> updateRecord records id state) ()
-
-addNewRecord :: Record -> IO ()
-addNewRecord r = updateRecords (\records -> r:records) ()
-
 inputLoop :: IO ()
 inputLoop = do
     putStrLn "Tell me what to do:"
     command <- getLine
     when (command == ":q") $ return ()
     let Input id targetState = read command :: Input
-    record <- loadRecord id
-    when (record & isNothing) $ return ()
+    state <- loadState id
+    when (state & isNothing) $ return ()
     let cmd = Command targetState
-    let r = fromJust record
-    let startState = toState r
+    let startState = fromJust state
     let endState = transition startState cmd
     if (endState & isNothing)
       then do
